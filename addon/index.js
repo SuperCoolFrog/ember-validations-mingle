9658e2488dce585864e55b570a3fe867f3ef11b5
Vasquez, Michael A <MAVasquez@pplweb.com>
2017-03-07 21:39:14 +0000

    Added support for object of functions

--- addon/index.js
+++ addon/index.js
@@ -1,5 +1,26 @@
 import mingleMessage from 'ember-validations-mingle/mingles/message';
+import Ember from 'ember';
+
+const { set } = Ember;
+
+const mingleObject = function(obj) {
+  let keys = Object.keys(obj);
+
+  return keys.reduce((mingled, k) => {
+    set(mingled, k, mingleMessage(obj[k]));
+    return mingled;
+  }, {});
+};
 
 export default function mingle(validation) {
-  return mingleMessage(validation);
+  let t = typeof validation;
+
+  switch(t) {
+    case 'function':
+      return mingleMessage(validation);
+    case 'object':
+      return mingleObject(validation);
+    default:
+      throw 'you must provide a validation function or array of functions';
+  }
 }
